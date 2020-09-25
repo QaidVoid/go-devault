79ca23870ca8c71208d2536d81d92c7034b8c3db
QaidVoid <12017109+QaidVoid@users.noreply.github.com>
2020-09-25 11:09:55 +0545

    Rough block/tx subscription methods

---
+++ events.go
@@ -0,0 +1,47 @@
+package devault
+
+import (
+	"log"
+	"time"
+)
+
+// SubscribeNewBlock subscribes to new blocks
+func (rpc *RPC) SubscribeNewBlock(c chan int64) {
+	b, err := rpc.GetBlockCount()
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastBlock := b
+
+	go func() {
+		for {
+			newBlock, err := rpc.GetBlockCount()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if newBlock > lastBlock {
+				lastBlock++
+				c <- lastBlock
+			}
+			time.Sleep(time.Second * 15)
+		}
+	}()
+}
+
+// SubscribeNewTransaction subscribes new transactions
+func (rpc *RPC) SubscribeNewTransaction(c chan string) {
+	go func() {
+		newblock := make(chan int64)
+		rpc.SubscribeNewBlock(newblock)
+
+		for height := range newblock {
+			hash, _ := rpc.GetBlockHash(height)
+			block, _ := rpc.GetBlock(hash)
+
+			for _, tx := range block.Transactions {
+				c <- tx
+			}
+		}
+	}()
+}
